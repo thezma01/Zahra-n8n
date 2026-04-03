1647dbdd99fbe880c2d37415e0ede843adda673f
thezma01 <zahrazma01@gmail.com>
2026-04-03 22:07:47 +0500

    AI Task: Create Product API in Laravel

---
+++ app/Models/Product.php
@@ -0,0 +1,16 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'product_name',
+        'product_summary',
+        'timestamp',
+    ];
+}
