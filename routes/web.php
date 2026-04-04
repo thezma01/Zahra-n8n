fef33344b2d22f5caf3a9d61be3cad2124b107bf
thezma01 <zahrazma01@gmail.com>
2026-04-04 18:32:56 +0500

    AI Task: Contact Page with Form and Map

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GetInTouchController;
+use App\Http\Controllers\ContactController;
 
-Route::get('/get-in-touch', [GetInTouchController::class, 'index']);
-Route::post('/get-in-touch', [GetInTouchController::class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
