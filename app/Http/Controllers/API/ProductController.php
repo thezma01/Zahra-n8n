e3cc0f34600e8c85f0e54020d7f02d4d4e61222b
thezma01 <zahrazma01@gmail.com>
2026-04-04 15:26:44 +0500

    AI Task: Product Model Update + API Setup

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,16 +3,21 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
     public function insert(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'price' => 'required|numeric',
             'quantity' => 'required|integer',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], 422);
+        }
 
         $product = Product::create($request->all());
 
