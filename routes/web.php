1adbc8246027edbb2266c2a812c8370df92f850a
thezma01 <zahrazma01@gmail.com>
2026-04-04 16:35:13 +0500

    AI Task: Create Product View with Cards

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
-<!-- web.php -->
+<?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::get('/products', function () {
-    $products = [
-        ['name' => 'Product 1', 'price' => 10.99, 'quantity' => 5],
-        ['name' => 'Product 2', 'price' => 9.99, 'quantity' => 10],
-        ['name' => 'Product 3', 'price' => 12.99, 'quantity' => 7],
-    ];
-
-    return view('products', compact('products'));
+    return view('products');
 });
