1866b63f97c2d31f2f4d3e2c98baada1ccd7fa7e
thezma01 <zahrazma01@gmail.com>
2026-04-04 15:34:33 +0500

    AI Task: Add Update and Delete Methods in ProductController

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::post('/products', [ProductController::class, 'create']);
-Route::get('/products', [ProductController::class, 'getAll']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'delete']);
