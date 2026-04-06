67b011c46e1113a840ab2c069cf21be8a2a38130
thezma01 <zahrazma01@gmail.com>
2026-04-06 22:05:57 +0500

    AI Task: backendapi

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,1 @@
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'delete']);
+Route::apiResource('shipments', \App\Http\Controllers\ShipmentController::class);
