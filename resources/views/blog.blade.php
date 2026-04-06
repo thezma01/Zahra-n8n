56715339c0d43b22eefffe599c2b4a55c1027bb1
thezma01 <zahrazma01@gmail.com>
2026-04-06 11:47:14 +0500

    AI Task: blog-page-ui-enhancement

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,28 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container mt-5">
-        <h1>Blog</h1>
-        <div class="row">
-            @foreach($blogPosts as $post)
-            <div class="col-md-4">
-                <div class="card mb-4">
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Blog</div>
                     <div class="card-body">
-                        <h5 class="card-title">{{ $post['title'] }}</h5>
-                        <p class="card-text">By {{ $post['author'] }} on {{ $post['date'] }}</p>
-                        <p class="card-text">{{ $post['excerpt'] }}</p>
+                        @foreach($blogs as $blog)
+                            <div class="blog-card">
+                                <h2>{{ $blog->title }}</h2>
+                                <p>{{ $blog->content }}</p>
+                                <a href="{{ route('blog.show', $blog->id) }}">Read More</a>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
-            @endforeach
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
