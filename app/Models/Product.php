05efbb905e7a24cb2863466a95da5da036dc57c2
thezma01 <zahrazma01@gmail.com>
2026-04-07 15:05:43 +0500

    AI Task: backendapi

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,7 +1,7 @@
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
