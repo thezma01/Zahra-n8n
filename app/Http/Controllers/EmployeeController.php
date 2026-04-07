c6f316a7174cfda7344aafaf79e38aa4581d3934
thezma01 <zahrazma01@gmail.com>
2026-04-07 16:10:53 +0500

    AI Task: backendapi

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,72 @@
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employees = Employee::all();
+        return response()->json($employees);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $employee = new Employee();
+        $employee->Emp_ID = $request->input('Emp_ID');
+        $employee->name = $request->input('name');
+        $employee->email = $request->input('email');
+        $employee->timestamp = $request->input('timestamp');
+        $employee->save();
+        return response()->json($employee, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $employee = Employee::find($id);
+        if (!$employee) {
+            return response()->json(['message' => 'Employee not found'], 404);
+        }
+        $employee->Emp_ID = $request->input('Emp_ID');
+        $employee->name = $request->input('name');
+        $employee->email = $request->input('email');
+        $employee->timestamp = $request->input('timestamp');
+        $employee->save();
+        return response()->json($employee);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $employee = Employee::find($id);
+        if (!$employee) {
+            return response()->json(['message' => 'Employee not found'], 404);
+        }
+        $employee->delete();
+        return response()->json(['message' => 'Employee deleted successfully']);
+    }
+}
