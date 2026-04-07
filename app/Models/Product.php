1cf8afbc582a7517a692cd510ea88d494a940d3a
thezma01 <zahrazma01@gmail.com>
2026-04-07 14:43:09 +0500

    AI Task: backendapi

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,16 +1,16 @@
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'product_id',
-        'product_name',
-        'product_summary',
+        'id',
+        'name',
+        'summary',
         'timestamp',
     ];
 }
