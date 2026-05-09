e8271cd3dd73ad2786be5568dc95c19327b7edc2
thezma01 <zahrazma01@gmail.com>
2026-05-09 12:21:06 +0500

    Delete src directory

--- src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/AuthController');
-
-router.post('/login', authController.login);
-router.post('/forgot-pin', authController.forgotPin);
-router.post('/reset-pin', authController.resetPin);
-
-module.exports = router;
